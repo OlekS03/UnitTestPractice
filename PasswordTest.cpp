7412a2eef9e3294213b936ea40061397537d7d75
Oleksandr Shestopalov <shestopalovaleks@gmail.com>
2025-11-05 11:51:43 -0800

    Updated Password tests

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -20,3 +20,53 @@ TEST(PasswordTest, single_letter_password)
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
 }
+
+TEST(PasswordTest, multiple_leading_characters)
+{
+    Password my_password;
+    int actual = my_password.count_leading_characters("AAAxyz");
+    ASSERT_EQ(3, actual);
+}
+
+TEST(PasswordTest, no_repeated_leading_characters)
+{
+    Password my_password;
+    int actual = my_password.count_leading_characters("Abbb");
+    ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, empty_string)
+{
+    Password my_password;
+    int actual = my_password.count_leading_characters("");
+    ASSERT_EQ(0, actual);
+}
+
+TEST(PasswordTest, case_sensitivity)
+{
+    Password my_password;
+    int actual = my_password.count_leading_characters("AAaAA");
+    ASSERT_EQ(2, actual);
+}
+
+TEST(PasswordTest, all_same_character)
+{
+    Password my_password;
+    int actual = my_password.count_leading_characters("ZZZZZZ");
+    ASSERT_EQ(6, actual);
+}
+
+TEST(PasswordTest, numeric_characters)
+{
+    Password my_password;
+    int actual = my_password.count_leading_characters("1111abc");
+    ASSERT_EQ(4, actual);
+}
+
+TEST(PasswordTest, special_characters)
+{
+    Password my_password;
+    int actual = my_password.count_leading_characters("$$$Hello");
+    ASSERT_EQ(3, actual);
+}
+
